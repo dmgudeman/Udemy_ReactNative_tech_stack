f50881658d69bed1990aecd3a2212e0f388324f7
davidmgudeman <davidmgudeman@gmail.com>
2017-09-05 08:15:35 -0700

    fixed the import of selectLibrary

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+    console.log(this.props);
+    
     return (
       <CardSection>
         <Text style={titleStyle}>
@@ -20,6 +24,6 @@ const styles = {
     fontSize: 18,
     paddingLeft: 15
   }
-}
+};
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
